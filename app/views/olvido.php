e1c6f349fdf9940c8bf69b18358714503ecad661
Alejandro <alexmonty6@gmail.com>
2023-11-07 22:59:41 +0100

    espaciado en la vista olvido

--- app/views/olvido.php
+++ app/views/olvido.php
@@ -1,16 +1,22 @@
 <?php include_once 'header.php' ?>
-<?php if(isset($data['errors']) && count($data['errors']) > 0): ?>
+<?php if (isset($data['errors']) && count($data['errors']) > 0): ?>
     <div class="alert alert-danger mt-3">
         <?php foreach ($data['errors'] as $value): ?>
-            <strong><?= $value ?></strong><br>
+            <strong>
+                <?= $value ?>
+            </strong><br>
         <?php endforeach; ?>
     </div>
 <?php endif ?>
 <div class="card p-4 bg-light">
     <div class="card-header">
-        <h1 class="text-center"><?= $data['subtitle'] ?></h1>
+        <h1 class="text-center">
+            <?= $data['subtitle'] ?>
+        </h1>
     </div>
     <div class="card-body">
+
+
         <form action="<?= ROOT ?>login/olvido" method="POST">
             <div class="form-group text-left mb-2">
                 <label for="email">Correo electrónico:</label>
